cf0ecc260b43d86bbc80f47fa8f823501a354eaa
dmd120997 <dmd120997@gmail.com>
2025-12-16 02:11:07 +0200

    Added filters logic

--- public/script.js
+++ public/script.js
@@ -3,13 +3,35 @@ const API_URL = "http://localhost:8080/todos";
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
+let allTodos = [];
+let currentFilter = "all";
 
 async function loadTodos() {
   const res = await fetch(API_URL);
-  const todos = await res.json();
-  todoList.innerHTML = "";
-  todos.forEach(addTodoToDOM);
+  allTodos = await res.json();
+  renderTodos();
 }
+
+function getFilteredTodos() {
+  if (currentFilter === "active") {
+    return allTodos.filter((todo) => !todo.completed);
+  }
+
+  if (currentFilter === "completed") {
+    return allTodos.filter((todo) => todo.completed);
+  }
+
+  return allTodos;
+}
+
+function renderTodos() {
+  todoList.innerHTML = "";
+
+  const todosToShow = getFilteredTodos();
+  todosToShow.forEach(addTodoToDOM);
+}
+
+
 
 function addTodoToDOM(todo) {
   const li = document.createElement("li");
@@ -57,12 +79,29 @@ async function toggleTodo(id, completed) {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ completed }),
   });
-  loadTodos();
+  const updatedTodo = await res.json();
+
+  const index = allTodos.findIndex(t => t.id === updatedTodo.id);
+  allTodos[index] = updatedTodo;
+
+  renderTodos();
 }
 
 async function deleteTodo(id) {
   await fetch(`${API_URL}/${id}`, { method: "DELETE" });
-  loadTodos();
+
+  allTodos = allTodos.filter(t => t.id !== id);
+  renderTodos();
 }
+
+const filterButtons = document.querySelectorAll(".filters button");
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    currentFilter = button.dataset.filter;
+    renderTodos();
+  });
+});
+
 
 loadTodos();
